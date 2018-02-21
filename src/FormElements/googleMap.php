69bea040bfa67e8d3c6499587d283f397dc0a453
php-dev <sserdyuk@webakula.ua>
2018-02-21 13:05:22 +0200

    Initial commit

--- src/FormElements/googleMap.php
+++ src/FormElements/googleMap.php
@@ -6,7 +6,7 @@ use KodiCMS\Assets\Facades\Meta;
 use SleepingOwl\Admin\Form\Element\NamedFormElement;
 
 
-class GoogleMap extends NamedFormElement {
+class googleMap extends NamedFormElement {
     public function initialize() {
         Meta::addJs('admin-custom-js-gmap-api', 'https://maps.googleapis.com/maps/api/js?key=' . env('GOOGLE_MAPS_API_KEY'));
         Meta::addJs('admin-custom-js-gmap-init', asset('vendor/webakula/js/google-map/google-map.js'));
